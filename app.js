6071e50c5e50598810296e2f2ba98c89ee1efcf2
BrilliantTreasure1 <brillianttrussure@gmail.com>
2025-06-04 11:26:35 +0330

    feat(middleware): add request counter middleware to log total requests

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const taskRoutes = require('./routes/tasks')
+const requestCounter = require('./middleware/requestCounter')
 
 app.use(express.json());
+
+app.use(requestCounter);
 
 app.use("/tasks" ,taskRoutes );
 
